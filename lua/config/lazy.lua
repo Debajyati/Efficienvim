be661e5fbc014e1f71989a538e860e23860382be
Debajyati Dey <127122455+Debajyati@users.noreply.github.com>
2023-12-18 20:22:08 +0530

    Update lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -698,7 +698,7 @@ require("lazy").setup({
     event='BufEnter',
     config = function ()
       -- Changed '<C-g>' here to '<C-y>' to make it work.
-      vim.keymap.set('i', '<C-y>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+      vim.keymap.set('i', '<C-y>', function() return vim.fn['codeium#Accept']() end, { expr = true })
       vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
       vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
       vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
