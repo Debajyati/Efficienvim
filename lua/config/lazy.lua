83b867b28031728e8fec5c725b5d7f9db4572bea
Debajyati <ddebajyati@gmail.com>
2023-12-22 01:41:11 +0530

    better vim-motions in terminal-mode

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -537,34 +537,6 @@ require("lazy").setup({
         add_exec(opts)
       end
 
-      require("toggleterm").setup {
-        size = 80, -- when changing terminal direction to horizontal, the terminal size needs to be decreased
-        open_mapping = [[<c-\>]],
-        hide_numbers = true, -- hide the number column in toggleterm buffers
-        shade_filetypes = {},
-        shade_terminals = true,
-        shading_factor = 2, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-        start_in_insert = true,
-        insert_mappings = true, -- whether or not the open mapping applies in insert mode
-        persist_size = false,
-        direction = "vertical", -- change this to "horizontal" or "float" if you prefer
-        close_on_exit = true, -- close the terminal window when the process exits
-        shell = nil, -- change the default shell
-        float_opts = {
-          border = "rounded",
-          winblend = 0,
-          highlights = {
-            border = "Normal",
-            background = "Normal",
-          },
-        },
-        winbar = {
-          enabled = true,
-          name_formatter = function(term) --  term: Terminal
-            return term.count
-          end,
-        },
-      }
       vim.cmd [[
           augroup terminal_setup | au!
           autocmd TermOpen * nnoremap <buffer><LeftRelease> <LeftRelease>i
@@ -582,10 +554,10 @@ require("lazy").setup({
 
       local opts = { noremap = true, silent = true }
       function _G.set_terminal_keymaps()
-        vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-        vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-        vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-        vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+        vim.api.nvim_buf_set_keymap(0, "t", "<C-n><C-h>", [[<C-\><C-n><C-W>h]], opts)
+        vim.api.nvim_buf_set_keymap(0, "t", "<C-n><C-j>", [[<C-\><C-n><C-W>j]], opts)
+        vim.api.nvim_buf_set_keymap(0, "t", "<C-n><C-k>", [[<C-\><C-n><C-W>k]], opts)
+        vim.api.nvim_buf_set_keymap(0, "t", "<C-n><C-l>", [[<C-\><C-n><C-W>l]], opts)
         vim.api.nvim_buf_set_keymap(0, "t", "<C-n>", [[<C-\><C-n>]], opts)
       end
     end
@@ -600,6 +572,7 @@ require("lazy").setup({
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
+    event="VeryLazy",
   },
   -- for better neovim built-in lsp experience
   {
