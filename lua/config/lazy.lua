ba307a53a5f68d0979d7ba7de5838b739f693ea7
Debajyati Dey <127122455+Debajyati@users.noreply.github.com>
2024-02-12 18:29:39 +0530

    removed unnecessary plugin & its config. More efficient

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -611,24 +611,7 @@ require("lazy").setup({
       'nvim-tree/nvim-web-devicons',     -- optional
     },
   },
-  -- for great git & github integration in neovim
-
-  {
-    "pwntester/octo.nvim",
-    event = "VeryLazy",
-    cmd = "Octo",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("octo").setup()
-    end,
-    keys = {
-      { "<leader>o", "<cmd>Octo<cr>", desc = "Octo" },
-    },
-  },
+  -- for great git integration in neovim
 
   {
     "sindrets/diffview.nvim",
