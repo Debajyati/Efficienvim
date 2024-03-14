5f463aa7c359b6a2d645ce5d9ac7623262ab4a45
Debajyati <ddebajyati@gmail.com>
2024-03-14 18:01:41 +0530

    clipboard support enhanced

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -33,15 +33,22 @@ vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 50
 vim.cmd("filetype plugin indent on")
 
--- Uncomment the below lines to copy text always with clipboard only.
--- in the 2nd line below xclip is used as default. If you use any other
--- clipboard manager, then you have change the lines accordingly.
--- With these lines you can efficiently copy texts selected in 
--- VISUAL mode to clipboard with Ctrl+C & you won't need registers.
+-- Always send text selected in VISUAL Mode either
+-- deleted or copied with registers to clipboard.
+vim.opt.clipboard:append("unnamedplus") -- Kind of failsafe and also convenient!!
 
--- vim.opt.clipboard:append("unnamedplus")
+-- Although the above command should be more than enough but if & only if 
+-- the above command doesn't work well and you want a custom keybinding,
+-- READ!! the next 5 lines and uncomment the commented vim option code.
+-- xclip is used as default. If you use any other clipboard manager,  
+-- then you have to change the command accordingly.
+-- In windows the clipboard input tool is Set-Clipboard.
+-- With this line you can efficiently send copied texts selected in 
+-- VISUAL mode to clipboard with your custom keybinding `Ctrl+C`.
+
 -- vim.api.nvim_set_keymap("v", "<C-c>", ":w !xclip -i -sel c<CR><CR>", { noremap = true })
 
 
 vim.cmd("set number")
 vim.cmd("set cursorline")
+
