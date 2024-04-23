62f3c8be8ee63bb40c16a58300b471c434b9f69b
Debajyati <ddebajyati@gmail.com>
2024-04-23 14:33:29 +0530

    optional neo-tree explorer
    You can enable only one file explorer at a time.
     By default neo-tree is not installed & oil.nvim is the default file-explorer in `EFFICIENVIM`.
     To switch explorers, comment out one and uncomment another.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -764,6 +764,56 @@ require("lazy").setup({
       require('treesj').setup({--[[ your config ]]})
     end,
   },
+
+-- NOTE : You can enable only one file explorer at a time.
+  -- By default neo-tree is not installed, and oil.nvim is the default file-explorer
+  -- in `EFFICIENVIM`, to change switch explorers, comment out one and uncomment another.
+  -- Below is the Neo-tree config commented out.
+
+-- neo-tree : A file explorer sidebar with tree view.
+--[[   {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+      {
+        's1n7ax/nvim-window-picker',
+        version = '2.*',
+        config = function()
+            require 'window-picker'.setup({
+                filter_rules = {
+                    include_current_win = false,
+                    autoselect_one = true,
+                    -- filter using buffer options
+                    bo = {
+                        -- if the file type is one of following, the window will be ignored
+                        filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+                        -- if the buffer type is one of following, the window will be ignored
+                        buftype = { 'terminal', "quickfix" },
+                    },
+            },
+        })
+        end,
+      },
+    },
+    config = function ()
+      -- If you want icons for diagnostic errors, you'll need to define them somewhere:
+      vim.fn.sign_define("DiagnosticSignError",
+        {text = " ", texthl = "DiagnosticSignError"})
+      vim.fn.sign_define("DiagnosticSignWarn",
+        {text = " ", texthl = "DiagnosticSignWarn"})
+      vim.fn.sign_define("DiagnosticSignInfo",
+        {text = " ", texthl = "DiagnosticSignInfo"})
+      vim.fn.sign_define("DiagnosticSignHint",
+        {text = "󰌵", texthl = "DiagnosticSignHint"})
+
+      vim.keymap.set('n', "<space>e",':Neotree reveal<cr>', {desc = "file explorer tree toggle"})
+    end
+  }, ]]
+
   -- file-explorer which makes edit file-system like a buffer
   {
     "stevearc/oil.nvim",
