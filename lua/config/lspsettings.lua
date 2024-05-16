6ba1c265e9493df5f8e581d232f36a7c8dcf7aad
Debajyati <ddebajyati@gmail.com>
2024-05-16 21:32:11 +0530

    Enhanced lua lsp support

--- lua/config/lspsettings.lua
+++ lua/config/lspsettings.lua
@@ -61,8 +61,28 @@ cmp.setup({
   },
 })
 
-lspconfig["lua_ls"].setup({
+lspconfig.lua_ls.setup {
     capabilities = lsp_capabilities,
+    on_init = function(client)
+      local path = client.workspace_folders[1].name
+      if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+        return
+      end
+      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+        runtime = {
+          -- Tell the language server which version of Lua you're using
+          -- (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT'
+        },
+        -- Make the server aware of Neovim runtime files
+        workspace = {
+          checkThirdParty = false,
+          library = {
+            vim.env.VIMRUNTIME
+          }
+        }
+      })
+    end,
     settings = {
         Lua = {
             -- make the language server recognize "vim" global
@@ -74,4 +94,4 @@ lspconfig["lua_ls"].setup({
             },
         },
     },
-})
+}
