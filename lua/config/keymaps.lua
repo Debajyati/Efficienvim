8891a47d367d58ab71fc224c5fec48dade39fc8a
Debajyati <ddebajyati@gmail.com>
2024-05-16 22:16:51 +0530

    Set the global mapleader at its correct position. Enabled live preview for when changing colorschemes.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,5 @@
 -- Add any additional keymaps here
 
--- setting global mapleader
-vim.g.mapleader = " "
-
 -- Don't uncomment the line below if are using Neotree or Oil.nvim plugin as file explorer
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc="file explorer"})
 
@@ -14,7 +11,14 @@ local telescope_builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, { desc = "find files" })
 vim.keymap.set('n', '<leader>r', telescope_builtin.oldfiles, {desc = "recent files"})
 vim.keymap.set('n', '<leader>gf', telescope_builtin.git_files, { desc = "git files" })
-vim.keymap.set('n', '<leader>cc', telescope_builtin.colorscheme, {desc = "change colorscheme"})
+vim.keymap.set(
+  'n',
+  '<leader>cc',
+  function ()
+    telescope_builtin.colorscheme({ enable_preview=true });
+  end,
+  {desc = "change colorscheme"}
+)
 vim.keymap.set('n', '<leader>bf', telescope_builtin.buffers, {desc = "find buffers"})
 vim.keymap.set('n', '<leader>ps', function()
   telescope_builtin.grep_string({ search = vim.fn.input("Grep > ")});
