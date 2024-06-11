a074474a2c9ee67a0b60f49181bbba059f589436
Debajyati <ddebajyati@gmail.com>
2024-06-11 22:28:21 +0530

    minor changes

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -177,7 +177,7 @@ require("lazy").setup({
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "javascript", "html" },
           sync_install = false,
           auto_install = true,
           highlight = { enable = true, additional_vim_regex_highlighting = false, },
