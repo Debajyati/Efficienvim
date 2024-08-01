1ce18f0e405594a32b48fcb9c5dd38d36498ccbc
Debajyati Dey <ddebajyati@gmail.com>
2024-08-01 15:37:31 +0530

    improvements on eslint_d default setup

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -991,13 +991,25 @@ require("lazy").setup({
       config = function()
         local null_ls = require("null-ls")
         null_ls.setup({
+          debug = true,
           sources = {
             -- null_ls.builtins.formatting.stylua,
             null_ls.builtins.formatting.prettier,
             null_ls.builtins.formatting.black,
-            require("none-ls.diagnostics.eslint_d"),
             require("none-ls-shellcheck.diagnostics"),
             require("none-ls-shellcheck.code_actions"),
+            require("none-ls.diagnostics.eslint_d").with({
+              condition = function(utls)
+                return utls.root_has_file({
+                  ".eslintrc.js",
+                  ".eslintrc.mjs",
+                  ".eslintrc.cjs",
+                  ".eslintrc.yaml",
+                  ".eslintrc.yml",
+                  ".eslintrc.json",
+                })
+              end,
+            }),
           },
         })
 
