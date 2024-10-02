dc04c62e1e43a7e9e57343143c2d7979cec43fbe
Debajyati Dey <ddebajyati@gmail.com>
2024-10-02 15:29:22 +0530

    feat: commented code of autocmd provided for setting transparency

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,5 +1,19 @@
--- setting colorscheme as default 
--- vim.cmd.colorscheme "catppuccin"
-vim.cmd.colorscheme "eldritch"
+-- TODO:  
+-- If you are using a transparent terminal and want Neovim to be transparent also,
+-- then Uncomment the commented block of code (--[[]]) below
 
--- Add any autocommands here below
+-- set transparent background for floating and normal windows by default
+
+--[[ vim.api.nvim_create_augroup("colortheme", { clear = true })
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  group    = "colortheme",
+  pattern  = "*",
+  callback = function()
+    vim.highlight.on_yank { timeout = 200 }
+    vim.api.nvim_set_hl(0, "Normal", { bg="none"})
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg="none" })
+  end
+}) ]]
+
+-- Add any custom autocommands here below
